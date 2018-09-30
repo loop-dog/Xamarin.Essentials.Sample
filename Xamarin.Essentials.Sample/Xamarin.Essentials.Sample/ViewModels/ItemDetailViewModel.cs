db6a5d8a55586be9b5581117e9184ca0ccd2b45f
loop_dog <loop_dog2000@hotmail.com>
2018-09-30 22:30:06 +0900

    プロジェクト ファイルを追加します。

---
+++ Xamarin.Essentials.Sample/Xamarin.Essentials.Sample/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin.Essentials.Sample.Models;
+
+namespace Xamarin.Essentials.Sample.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
