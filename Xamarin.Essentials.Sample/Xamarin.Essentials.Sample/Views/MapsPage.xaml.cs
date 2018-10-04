8b1952f77f53d24cba4cfd6b1245f38c9ad57c0c
loop_dog <loop_dog2000@hotmail.com>
2018-10-05 02:05:02 +0900

    ●自作アプリ開発 ◎Xamarin.Essentials試行 ・マップ新規追加

---
+++ Xamarin.Essentials.Sample/Xamarin.Essentials.Sample/Views/MapsPage.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xamarin.Essentials.Sample.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MapsPage : ContentPage
+    {
+        public MapsPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void BtnOpenWithLocation_ClickedAsync(object sender, EventArgs e)
+        {
+            try
+            {
+                Double.TryParse(EdtLat.Text, out var lat);
+                Double.TryParse(EdtLon.Text, out var lon);
+
+                var location = new Location(lat, lon);
+                var options = new MapsLaunchOptions { Name = EdtMapsLaunchOptions1.Text };
+
+                await Maps.OpenAsync(location, options);
+
+                //ret = $"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}";
+                //Console.WriteLine(ret);
+                //this.lblResult.Text = ret;
+            }
+            catch (Exception ex)
+            {
+                // Unable to get location
+                this.lblResult.Text = ex.ToString();
+            }
+        }
+
+        private async void BtnOpenWithPlacemark_ClickedAsync(object sender, EventArgs e)
+        {
+            try
+            {
+                var ret = "";
+                var placemark = new Placemark
+                {
+                    CountryName = EdtCountryName2.Text,
+                    AdminArea = EdtAdminArea2.Text,
+                    Thoroughfare = EdtThoroughfare2.Text,
+                    Locality = EdtLocality2.Text
+                };
+                var options = new MapsLaunchOptions { Name = EdtMapsLaunchOptions2.Text };
+
+                await Maps.OpenAsync(placemark, options);
+
+                //ret = $"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}";
+                //Console.WriteLine(ret);
+                //this.lblResult.Text = ret;
+            }
+            catch (Exception ex)
+            {
+                // Unable to get location
+                this.lblResult.Text = ex.ToString();
+            }
+        }
+
+        private async void BtnPlacemark_OpenMaps_ClickedAsync(object sender, EventArgs e)
+        {
+            try
+            {
+                var ret = "";
+                var placemark = new Placemark
+                {
+                    CountryName = EdtCountryName3.Text,
+                    AdminArea = EdtAdminArea3.Text,
+                    Thoroughfare = EdtThoroughfare3.Text,
+                    Locality = EdtLocality3.Text
+                };
+
+                //var options = new MapsLaunchOptions { Name = "Microsoft Building 25" };
+                await placemark.OpenMapsAsync();
+
+                //ret = $"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}";
+                //Console.WriteLine(ret);
+                //this.lblResult.Text = ret;
+            }
+            catch (Exception ex)
+            {
+                // Unable to get location
+                this.lblResult.Text = ex.ToString();
+            }
+
+        }
+    }
+}
