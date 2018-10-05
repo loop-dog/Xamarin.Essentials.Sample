733844833871237f8a49abd7fa8a8b0f55c88bea
loop_dog <loop_dog2000@hotmail.com>
2018-10-05 23:56:17 +0900

    ●自作アプリ開発 ◎Xamarin.Essentials試行 ・ジャイロスコープ新規追加 ・磁力計新規追加

---
+++ Xamarin.Essentials.Sample/Xamarin.Essentials.Sample/Views/GyroscopePage.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xamarin.Essentials.Sample.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GyroscopePage : ContentPage
+    {
+        // Set speed delay for monitoring changes.
+        SensorSpeed speed = SensorSpeed.UI;
+
+        public GyroscopePage()
+        {
+            InitializeComponent();
+
+            // Register for reading changes.
+            Gyroscope.ReadingChanged += Gyroscope_ReadingChanged;
+        }
+
+        private void BtnStart_Clicked(object sender, EventArgs e)
+        {
+            this.ToggleGyroscope();
+        }
+
+        private void BtnStop_Clicked(object sender, EventArgs e)
+        {
+            this.ToggleGyroscope();
+        }
+
+        private void BtnClear_Clicked(object sender, EventArgs e)
+        {
+            lblResult.Text = "";
+        }
+
+
+        void Gyroscope_ReadingChanged(object sender, GyroscopeChangedEventArgs e)
+        {
+            var data = e.Reading;
+            // Process Angular Velocity X, Y, and Z
+            var ret=$"Reading: X: {data.AngularVelocity.X}, Y: {data.AngularVelocity.Y}, Z: {data.AngularVelocity.Z}";
+            Console.WriteLine(ret);
+            this.lblResult.Text += ret + Environment.NewLine;
+        }
+
+        public void ToggleGyroscope()
+        {
+            try
+            {
+                if (Gyroscope.IsMonitoring)
+                {
+                    Gyroscope.Stop();
+                    this.lblResult.Text += "Stop!!!" + Environment.NewLine;
+                }
+                else
+                {
+                    Gyroscope.Start(speed);
+                    this.lblResult.Text = "";
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Feature not supported on device
+                this.lblResult.Text = fnsEx.ToString();
+            }
+            catch (Exception ex)
+            {
+                // Other error has occurred.
+                this.lblResult.Text = ex.ToString();
+            }
+        }
+    }
+}
---
+++ Xamarin.Essentials.Sample/Xamarin.Essentials.Sample/Views/MagnetometerPage.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xamarin.Essentials.Sample.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MagnetometerPage : ContentPage
+    {
+        // Set speed delay for monitoring changes.
+        SensorSpeed speed = SensorSpeed.UI;
+
+        public MagnetometerPage()
+        {
+            InitializeComponent();
+
+            // Register for reading changes.
+            Magnetometer.ReadingChanged += Magnetometer_ReadingChanged;
+
+        }
+
+        private void BtnStart_Clicked(object sender, EventArgs e)
+        {
+            this.ToggleMagnetometer();
+        }
+
+        private void BtnStop_Clicked(object sender, EventArgs e)
+        {
+            this.ToggleMagnetometer();
+        }
+
+        private void BtnClear_Clicked(object sender, EventArgs e)
+        {
+            lblResult.Text = "";
+        }
+
+        void Magnetometer_ReadingChanged(object sender, MagnetometerChangedEventArgs e)
+        {
+            var data = e.Reading;
+            // Process MagneticField X, Y, and Z
+            var ret=$"Reading: X: {data.MagneticField.X}, Y: {data.MagneticField.Y}, Z: {data.MagneticField.Z}";
+            Console.WriteLine(ret);
+            this.lblResult.Text += ret + Environment.NewLine;
+        }
+
+        public void ToggleMagnetometer()
+        {
+            try
+            {
+                if (Magnetometer.IsMonitoring)
+                {
+                    Magnetometer.Stop();
+                    this.lblResult.Text += "Stop!!!" + Environment.NewLine;
+                }
+                else
+                {
+                    Magnetometer.Start(speed);
+                    this.lblResult.Text = "";
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Feature not supported on device
+                this.lblResult.Text = fnsEx.ToString();
+            }
+            catch (Exception ex)
+            {
+                // Other error has occurred.
+                this.lblResult.Text = ex.ToString();
+            }
+        }
+    }
+}
